ddd208ebcb4915c353d988e41a3e9c9335cf04f1
Oscar Stanchi <indirivacua@hotmail.com>
2025-07-11 20:09:17 -0300

    fixes #7

--- src/scripts/content/content.js
+++ src/scripts/content/content.js
@@ -1,5 +1,8 @@
-currentUrl = window.location.href;
-pubType = window.FINDEX[new URL(currentUrl).pathname];
+const currentUrl = window.location.href;
+const pubType = window.FINDEX[new URL(currentUrl).pathname];
+
+const hasPrecargaButton = [...document.querySelectorAll('input[type="submit"][name="btnSubmit"]')]
+    .some(btn => btn.value.toLowerCase().includes("precarga"));
 
 const datosBasicosTd = document.querySelector('td.CformRowHeader');
 if (datosBasicosTd && pubType !== undefined
@@ -7,19 +10,21 @@ if (datosBasicosTd && pubType !== undefined
     const importButton = globalThis.createImportButton(pubType);
     datosBasicosTd.style.position = "relative"; // Necesario para la superposición
     datosBasicosTd.appendChild(importButton);
+    hasPrecargaButton && (importButton.onclick = () => alert("Presiona el botón OMITIR"));
 }
 
 const guardarButton = document.querySelector('input[value="Modificar"]');
 if (guardarButton && pubType !== undefined) {
-    exportButton = globalThis.createExportButton(pubType);
+    const exportButton = globalThis.createExportButton(pubType);
     guardarButton.parentNode.insertBefore(exportButton, guardarButton.nextSibling);
     guardarButton.insertAdjacentHTML("afterend", " &nbsp; ");
+    hasPrecargaButton && (exportButton.onclick = () => alert("Presiona el botón EDITAR DATOS PRECARGADOS"));
 }
 
 chrome.storage.local.get(
     { institution: "1", password: "", autosubmit: true },
     (config) => {
-        const hasValidationError = document.querySelector('.form-section-errors.alert-danger');
+        const hasValidationError = document.querySelector('.form-section-errors');
         if (hasValidationError || config.password === "") return;
         const labels = document.getElementsByClassName("CformNombre");
         if (!labels.length || labels[0].textContent !== "Institución:") return;
