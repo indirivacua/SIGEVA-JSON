fba56cbdd39a88e07d4faab4b21c28603a7c7b76
Oscar Stanchi <indirivacua@hotmail.com>
2025-07-17 14:16:40 -0300

    fix journal import

--- src/scripts/content/findex.js
+++ src/scripts/content/findex.js
@@ -1,6 +1,7 @@
 window.FINDEX = {
     "/eva/bcoProduccionArticuloPublicacion.do": "revista",
     "/eva/bcoPrecargarArticulo.do": "revista",
+    "/eva/bcoBuscarRevistaPublicacion.do": "revista",
     "/eva/bcoProduccionListaPublicacionCapituloLibro.do": "capitulo",
     "/eva/bcoProduccionListaPublicacionCongreso.do": "congreso",
 };
