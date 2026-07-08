b40b4fdc33ec4d17cf2d8b1c3c646a933ad6f7f3
Oscar Stanchi <indirivacua@hotmail.com>
2026-07-08 12:05:24 -0300

    fix congress & book import

--- src/scripts/content/findex.js
+++ src/scripts/content/findex.js
@@ -2,6 +2,8 @@ window.FINDEX = {
     "/eva/bcoProduccionArticuloPublicacion.do": "revista",
     "/eva/bcoPrecargarArticulo.do": "revista",
     "/eva/bcoBuscarRevistaPublicacion.do": "revista",
+    "/eva/bcoProduccionCapituloLibro.do": "capitulo",
     "/eva/bcoProduccionListaPublicacionCapituloLibro.do": "capitulo",
+    "/eva/bcoProduccionCongreso.do": "congreso",
     "/eva/bcoProduccionListaPublicacionCongreso.do": "congreso",
 };
